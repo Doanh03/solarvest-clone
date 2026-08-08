c8e2de7928541c0967d2b267a08766d54ef26d3b
Trần In Đoanh <tranindoanh111@gmail.com>
2026-08-08 16:29:30 +0700

    Hoan thanh responsive Hero, Stats, Services, Projects, Contact

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 // SunPower Việt - script.js
 console.log("Trang web đã load thành công!");
+// Menu Hamburger
+const menuToggle = document.querySelector("#menuToggle");
+const navbar = document.querySelector("#navbar");
+
+menuToggle.addEventListener("click", function() {
+    navbar.classList.toggle("active");
+});
+
+// Form Validation
+const form = document.querySelector("#contactForm");
+const errorText = document.querySelector("#formError");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const ten = document.querySelector("#inputName").value;
+    const email = document.querySelector("#inputEmail").value;
+    const phone = document.querySelector("#inputPhone").value;
+
+    if (ten === "") {
+        errorText.style.color = "red";
+        errorText.textContent = "Vui lòng nhập họ tên";
+    } else if (!email.includes("@")) {
+        errorText.style.color = "red";
+        errorText.textContent = "Email không hợp lệ";
+    } else if (phone.length < 9) {
+        errorText.style.color = "red";
+        errorText.textContent = "Số điện thoại không hợp lệ";
+    } else {
+        errorText.style.color = "green";
+        errorText.textContent = "Gửi thành công! Cảm ơn bạn đã liên hệ.";
+        form.reset();
+    }
+});
